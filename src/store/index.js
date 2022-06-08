e781e0b009aa2b3a299ecc9425de967b3e5ca9d6
di-hwang <dihwang@aim.co.kr>
2022-06-08 16:14:02 +0900

    2022.06.08 Refactoring High Order Component Complete

--- src/store/index.js
+++ src/store/index.js
@@ -7,21 +7,13 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
 	state: {
-		news: [],
-		jobs: [],
-		ask: [],
 		user: {},
 		item: {},
+		list: [],
 	},
 	getters: {
-		fetchedAsk(state) {
-			return state.ask;
-		},
 		fetchedItem(state) {
 			return state.item;
-		},
-		fetchedJobs(state) {
-			return state.jobs;
 		},
 	},
 	mutations,
